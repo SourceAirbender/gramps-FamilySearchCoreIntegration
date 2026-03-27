e40f1aeb2743d25689dc1930edd493471c09049b
SourceAirbender <grios019@gmail.com>
2026-03-27 03:01:31 +0000

    reverted removal of empty lines in some files & reintroduced the # in grampsgui.py

--- gramps/gui/grampsgui.py
+++ gramps/gui/grampsgui.py
@@ -4,7 +4,7 @@
 # Copyright (C) 2000-2006  Donald N. Allingham
 # Copyright (C) 2009 Benny Malengier
 # Copyright (C) 2018 Paul Culley
-
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
